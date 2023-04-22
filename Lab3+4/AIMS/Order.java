c589506cd9e44fb1b9a471e133811cc7589767ef
SinnerSS <ntnbusinf@gmail.com>
2023-04-22 21:48:46 +0700

    added date attribute and print method

--- Lab3+4/AIMS/Order.java
+++ Lab3+4/AIMS/Order.java
@@ -1,9 +1,35 @@
 public class Order {
     public static final int MAX_NUMBER_ORDER = 10;
+    public static final int MAX_LIMITED_ORDER = 5;
+
+    public static int nbOrders = 0;
 
     private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBER_ORDER];
 
     private int qtyOrdered = 0;
+    private String dateOrdered;
+
+    
+
+    public Order() {
+        if(nbOrders == MAX_LIMITED_ORDER) throw new IllegalStateException();
+        this.dateOrdered = null;
+        nbOrders += 1;
+    }
+
+    public Order(String dateOrdered) {
+        if(nbOrders == MAX_LIMITED_ORDER) throw new IllegalStateException();
+        this.dateOrdered = dateOrdered;
+        nbOrders += 1;
+    }
+
+    public String getDateOrdered() {
+        return dateOrdered;
+    }
+
+    public void setDateOrdered(String dateOrdered) {
+        this.dateOrdered = dateOrdered;
+    }
 
     public int getQtyOrdered() {
         return qtyOrdered;
@@ -70,6 +96,33 @@ public class Order {
             totalCost += itemsOrdered[i].getCost();
         }
         return totalCost;
+    }
+
+    public void print() {
+
+        System.out.println("*********************Order****************");
+        System.out.printf("Date: %s\n", dateOrdered);
+        System.out.println("Ordered Items:");
+        
+
+        for(int i=0; i < qtyOrdered; i++) {
+    
+            String title = itemsOrdered[i] != null ? itemsOrdered[i].getTitle() : "N/A";
+            String category = itemsOrdered[i] != null ? itemsOrdered[i].getCategory() : "N/A";
+            String director = itemsOrdered[i] != null ? itemsOrdered[i].getDirector() : "N/A";
+            int length = itemsOrdered[i] != null ? itemsOrdered[i].getLength() : 0;
+            double price = itemsOrdered[i] != null ? itemsOrdered[i].getCost() : 0;
+            
+            System.out.printf("%d. DVD - %s - %s - %s - %d min: %.2f $\n",
+                i + 1, title, category, director, length, price);
+        }
+        
+
+        double totalCost = this.totalCost();
+        System.out.printf("Total cost: %.2f\n", totalCost);
+        
+
+        System.out.println("*******************************************");
     }
     
 }
