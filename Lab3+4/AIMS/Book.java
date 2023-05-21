adefefcf27eb6424d471883bfe3824d292634a95
SinnerSS <ntnbusinf@gmail.com>
2023-05-21 18:30:04 +0700

    first session

---
+++ Lab3+4/AIMS/Book.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book extends Media {
+    private List<String> authors = new ArrayList<String>();
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public boolean addAuthor(String authorName) {
+        if(authors.contains(authorName)) return false;
+        authors.add(authorName);
+        return true;
+    }
+
+    public boolean removeAuthor(String authorName) {
+        if(!authors.contains(authorName)) return false;
+        authors.remove(authorName);
+        return true;
+    }
+    
+}
