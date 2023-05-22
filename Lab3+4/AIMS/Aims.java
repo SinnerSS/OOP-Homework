ed228ba65a2ba3c6f09f4812a0586ec06f0cc7a8
SinnerSS <ntnbusinf@gmail.com>
2023-05-23 00:18:26 +0700

    refactor to use cli

--- Lab3+4/AIMS/Aims.java
+++ Lab3+4/AIMS/Aims.java
@@ -1,59 +1,344 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.IOException;
+
 public class Aims {
 
+    //TODO: refactor to use hashmap for orderlist
+    private static List<Order> orderList = new ArrayList<>();
+    private static Scanner inputScan = new Scanner(System.in);
+
+
+    private static final List<Class<?>> mediaType = new ArrayList<>(List.of(Book.class, DigitalVideoDisc.class));
+
     public static void main(String[] args) {
-        Order order1 = new Order();
+        int selection = 0;
 
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King");
-        dvd1.setCategory("Animation");
-        dvd1.setCost(19.95f);
-        dvd1.setDirector("Roger Allers");
-        dvd1.setLength(87);
+        loop: while(true) {
+            selection = showMenu();
 
-        order1.addMedia(dvd1);
 
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars");
-        dvd2.setCategory("Science Fiction");
-        dvd2.setCost(24.95f);
-        dvd2.setDirector("George Lucas");
-        dvd2.setLength(124);
+            switch(selection) {
+                case 1:
+                    createOrder();
+                    break;
 
-        order1.addMedia(dvd2);
+                
+                case 2:
+                    if(orderList.isEmpty()) {
+                        clrscr();
 
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin");
-        dvd3.setCategory("Animation");
-        dvd3.setCost(18.99f);
-        dvd3.setDirector("John Musker");
-        dvd3.setLength(90);
 
-        order1.addMedia(dvd3);
+                        System.out.println("No order exist");        
+                        System.out.print("Press enter to continue");
+                        inputScan.nextLine();
+                        break;
+                    }
 
-        System.out.print("Total cost is: ");
-        System.out.printf("%.2f\n",order1.totalCost());
 
-        order1.removeMedia(dvd1);
-        System.out.print("Total cost after removing " + dvd1.getTitle() + " is: ");
-        System.out.printf("%.2f\n",order1.totalCost());
+                    Media media = createItem();
+                    addItemToOrder(media);
+                    break;
 
-        DigitalVideoDisc dvd4 = new DigitalVideoDisc("The Avengers", "Action", "Joss Whedon", 143, 24.95f);
-        DigitalVideoDisc dvd5 = new DigitalVideoDisc("Inception", "Christopher Nolan", "Action", 135, 9.99f);
-        DigitalVideoDisc dvd6 = new DigitalVideoDisc("The Shawshank Redemption", "Frank Darabont", "Drama", 142, 7.99f);
 
-        Order order2 = new Order("Feb 2nd 2022");
-        Order order3 = new Order("Aug 15th 2022");
-    
-        order2.addMedia(dvd4);
-        order2.addMedia(dvd6);
-    
-        order3.addMedia(dvd3);
-        order3.addMedia(dvd5);
-    
+                case 3:
+                    if(orderList.isEmpty()) {
+                        clrscr();
 
-        System.out.println("Order 1:");
-        order1.print();
-        System.out.println("Order 2:");
-        order2.print();
-        System.out.println("Order 3:");
-        order3.print();
+
+                        System.out.println("No order exist");        
+                        System.out.print("Press enter to continue");
+                        inputScan.nextLine();
+                        break;
+                    }
+
+                    removeItemFromOrder();
+                    break;
+
+
+                case 4:
+                    if(orderList.isEmpty()) {
+                        clrscr();
+
+
+                        System.out.println("No order exist");        
+                        System.out.print("Press enter to continue");
+                        inputScan.nextLine();
+                        break;
+                    }
+
+                    clrscr();
+                    long orderId = getOrderId();
+                    
+
+                    for(Order order : orderList) {
+                        clrscr();
+                        if(order.getId() == orderId) {
+                            order.print();
+                            System.out.print("Press enter to continue");
+                            inputScan.nextLine();
+                        }
+                    }
+                    break;
+
+                case 0: 
+                    break loop;
+
+
+            }
+        }
+
+
+        inputScan.close();
+
 
     }
+
+
+    public static int showMenu() {
+        clrscr();
+
+
+        System.out.println("Order Management Application: ");
+        System.out.println("--------------------------------");
+        System.out.println("1. Create new order");
+        System.out.println("2. Add item to the order");
+        System.out.println("3. Delete item by id");
+        System.out.println("4. Display the items list of order");
+        System.out.println("0. Exit");
+        System.out.println("--------------------------------");
+        System.out.println("Please choose a number: 0-1-2-3-4");
+
+
+        int selection = inputScan.nextInt();
+        inputScan.nextLine();
+
+
+        return selection;
+
+
+    }
+
+
+    private static void createOrder() {
+        Order order;
+
+
+        clrscr();
+
+
+        System.out.print("Enter order date: ");
+        String dateOrder = inputScan.nextLine();
+
+        if(dateOrder.isEmpty())
+            order = new Order();
+        else
+            order = new Order(dateOrder);
+        orderList.add(order);
+
+
+        clrscr();
+        System.out.println("Order " + order.getId() + " has been added");
+        System.out.print("Press enter to continue");
+        inputScan.nextLine();
+
+
+    }
+ 
+
+    private static Media createItem() {
+        int selection = 0;
+        Media media = null;
+    
+
+        clrscr();
+
+
+        System.out.println("Choose item type:");
+        System.out.println("--------------------------------");
+        System.out.println("1. Book");
+        System.out.println("2. Dvd");
+
+
+        selection = inputScan.nextInt();
+        inputScan.nextLine();
+
+
+        clrscr();
+
+
+        System.out.print("Enter title: ");
+        String title = inputScan.nextLine();
+
+
+        System.out.print("Enter category: ");
+        String category = inputScan.nextLine();
+
+
+        System.out.print("Enter cost: ");
+        int cost = inputScan.nextInt();
+        inputScan.nextLine();
+
+
+        switch(selection) {
+            case 1:
+                List<String> authorsList = new ArrayList<>();
+                String author;
+
+
+                do {
+                    System.out.print("Add author: ");
+                    author = inputScan.nextLine();
+
+                    if(!author.isEmpty())
+                        authorsList.add(author);
+                }   while(!author.isEmpty());
+
+
+                Book book = new Book(title, category, authorsList, cost);
+                media = book;
+                
+
+                clrscr();
+                System.out.println("Item " + book.getId() + " has been created");
+                break;
+
+
+            case 2:
+                System.out.print("Enter director: ");
+                String director = inputScan.nextLine();
+
+
+                System.out.print("Enter length: ");
+                int length = inputScan.nextInt();
+                inputScan.nextLine();
+
+
+                DigitalVideoDisc dvd = new DigitalVideoDisc(title, category, director, length, cost);
+                media = dvd;
+
+
+                clrscr();
+                System.out.println("Item " + dvd.getId() + " has been created");
+                break;
+
+            
+        }
+
+
+        System.out.print("Press enter to continue");
+        inputScan.nextLine();
+        
+
+        return media;
+
+
+    }
+
+
+    private static void addItemToOrder(Media media) {
+        clrscr();
+
+
+        long id = getOrderId();
+
+
+        for(Order order : orderList) {
+            if(order.getId() == id) {
+                order.addMedia(media);
+
+
+                clrscr();
+
+
+                System.out.println("Item " + media.getId() + " has been added to order " + order.getId());
+                System.out.println("Press enter to continue");
+                inputScan.nextLine();
+
+
+            }
+        }
+    }
+
+
+    private static void removeItemFromOrder() {
+        clrscr();
+
+
+        long orderId = getOrderId();
+
+
+        for(Order order : orderList) {
+            if(order.getId() == orderId) {
+                clrscr();
+
+
+                System.out.println("Items list of " + order.getId() + " :");
+                for(Media item : order.getItemsOrdered()) {
+                    String typeName = null;
+
+
+                    for(Class<?> type : mediaType) {
+                        if(type.isInstance(item)) {
+                            typeName = type.getName();
+                            break;
+                        }
+                    }
+
+
+                    System.out.println(typeName + ": Id " + item.getId() + " with title " + item.getTitle());
+                }
+                System.out.println("--------------------------------");
+                System.out.print("Select an item Id to remove: ");
+                long itemId = inputScan.nextLong();
+                inputScan.nextLine();
+
+
+                for(Media item : order.getItemsOrdered())
+                    if(item.getId() == itemId) {
+                        order.removeMedia(item);
+                        break;
+                    }
+
+                clrscr();
+                System.out.println("Remove item " + itemId + " from order " + order.getId());
+                System.out.println("Press enter to continue");
+                inputScan.nextLine();                
+                
+
+                return;
+
+
+            }
+        }
+    }
+
+
+    private static long getOrderId() {
+        System.out.println("Order Id: ");
+        for(Order order : orderList) {
+            System.out.println("Id " + order.getId() + " created at " + (order.getDateOrdered() != null ? order.getDateOrdered() : "N/A"));
+        }
+
+        System.out.println("--------------------------------");
+        System.out.print("Select an order Id: ");
+        long id = inputScan.nextLong();
+        inputScan.nextLine();
+
+        return id;
+    }
+
+
+    private static void clrscr(){
+        try {
+            if (System.getProperty("os.name").contains("Windows"))
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            else
+                Runtime.getRuntime().exec("clear");
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
