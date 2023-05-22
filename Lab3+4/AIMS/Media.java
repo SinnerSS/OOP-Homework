a077ce872e31a4b6881539ec6529ceaafe16d19a
SinnerSS <ntnbusinf@gmail.com>
2023-05-23 00:16:54 +0700

    add id

--- Lab3+4/AIMS/Media.java
+++ Lab3+4/AIMS/Media.java
@@ -1,7 +1,14 @@
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Media {
     private String title = null;
     private String category = null;
     private float cost = 0;
+    static final AtomicLong NEXT_ID = new AtomicLong(1000);
+    final long id = NEXT_ID.getAndIncrement();
+    public long getId() {
+        return id;
+    }
     public String getTitle() {
         return title;
     }
